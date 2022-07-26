ccf8d450e978c1fd37947269c4c0a181d0c89e09
mjhong0708 <mjhong0708@naver.com>
2022-07-26 13:46:05 +0900

    updated get_stable_phase to consider structures above hull

--- compass/matproj/api.py
+++ compass/matproj/api.py
@@ -3,13 +3,30 @@ from mp_api import MPRester
 from . import API_KEY
 
 
-def get_stable_phase(chemsys: str, fields: Optional[List[str]] = None, return_only_structure: bool = False, **kwargs):
+def get_stable_phase(
+    chemsys: str,
+    max_e_above_hull: float = 0.0,
+    return_only_structure: bool = False,
+    fields: Optional[List[str]] = None,
+    **kwargs,
+):
     if return_only_structure:
         if "structure" not in fields:
             fields.append("structure")
+
     with MPRester(API_KEY) as mpr:
         docs = mpr.summary.search(chemsys=chemsys, fields=fields, **kwargs)
         if len(docs) == 0:
             return None
-    docs.sort(key=lambda x: x.energy_above_hull)
-    return docs[0].structure if return_only_structure else docs[0]
+
+    def is_stable(doc):
+        E_above_hull = doc.energy_above_hull
+        if max_e_above_hull == 0.0:
+            return doc.is_stable
+        else:
+            return E_above_hull <= max_e_above_hull
+
+    stable_docs = filter(is_stable, docs)
+    if return_only_structure:
+        return [d.structure for d in stable_docs]
+    return list(stable_docs)
