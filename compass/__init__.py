a16d89f5d6ed0c0149e29019402cb2949b7939e0
mjhong0708 <mjhong0708@naver.com>
2022-09-20 11:34:51 +0900

    removed deleted import

--- compass/__init__.py
+++ compass/__init__.py
@@ -1,7 +1,6 @@
-from compass._src import units
 from compass._src.io import load_json, load_pickle, save_json, save_pickle
 from compass._src.logger import log
-from compass._src.misc import check_installed
+from compass._src.util import check_installed
 
 __all__ = [
     "save_json",
